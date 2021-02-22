8b83d39a54abb9413ce87c4d427547f4d62253bb
tszhong0411 <lainelson411@gmail.com>
2021-02-22 08:03:31 +0800

    Update language

--- login.php
+++ login.php
@@ -6,6 +6,6 @@ if ($_POST['username'] == $username && $_POST['password'] == $password ) {
     echo "Sign in suceesfully";
 } else {
     echo "Login failed. Redirect back to homepage after 3 seconds";
-    header( "refresh:3;url=https://tszhong-api.avosapps.us/" );
+    header( "refresh:3;url=https://tszhong-login.avosapps.us/" );
 }
 ?>
